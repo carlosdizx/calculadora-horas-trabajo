84436d45cdf13a76535b9f45a351e45fd83f8e46
Carlosdizx <carlodiaz@umariana.edu.co>
2022-02-12 22:40:28 -0500

    creado el dao de reporte y modificado el tipo de la llave primaria de la entidad

--- src/main/java/ias/modelos/Reporte.java
+++ src/main/java/ias/modelos/Reporte.java
@@ -11,7 +11,7 @@ import java.util.Date;
 public class Reporte {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
 
     @NotNull(message = "El tecnico no puede estar nulo")
     @NotEmpty(message = "El técnico no puede estar vacio")
@@ -31,4 +31,44 @@ public class Reporte {
     @NotNull(message = "La fecha de finalización no puede estar nulo")
     @NotEmpty(message = "La fecha de finalización no puede estar vacio")
     private Date fecha_fino;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTecnico() {
+        return tecnico;
+    }
+
+    public void setTecnico(String tecnico) {
+        this.tecnico = tecnico;
+    }
+
+    public ListaServicios getServicio() {
+        return servicio;
+    }
+
+    public void setServicio(ListaServicios servicio) {
+        this.servicio = servicio;
+    }
+
+    public Date getFecha_inicio() {
+        return fecha_inicio;
+    }
+
+    public void setFecha_inicio(Date fecha_inicio) {
+        this.fecha_inicio = fecha_inicio;
+    }
+
+    public Date getFecha_fino() {
+        return fecha_fino;
+    }
+
+    public void setFecha_fino(Date fecha_fino) {
+        this.fecha_fino = fecha_fino;
+    }
 }
---
+++ src/main/java/ias/repositories/ReporteDAO.java
@@ -0,0 +1,7 @@
+package ias.repositories;
+
+import ias.modelos.Reporte;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReporteDAO extends JpaRepository<Reporte, Long> {
+}
