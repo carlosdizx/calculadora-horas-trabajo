db8d36ec1b57e13c850304ea7e9724faf9a0b12d
Carlosdizx <carlodiaz@umariana.edu.co>
2022-02-19 22:54:37 -0500

    Funcionando la separación de horas por los distintos tipos que hay

--- src/main/java/ias/models/Calculo.java
+++ src/main/java/ias/models/Calculo.java
@@ -1,15 +1,22 @@
 package ias.models;
 
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class Calculo {
-    private double horas;
+
+    private final static int HORA_MINIMA = 7;
+
+    private final static int HORA_MAXIMA = 20;
+
+    private double normales;
 
     private double nocturnas;
 
     private double dominicales;
 
-    private double horasExtra;
+    private double normalesExtra;
 
     private double nocturnasExtra;
 
@@ -19,33 +26,98 @@ public class Calculo {
 
     final private Date fin;
 
+    private final double minutosTrabjados;
+
     private final double horasTrabajadas;
 
-    public Calculo(final Date pInicio, final Date pFin) {
-        horas = 0;
+    private final long datosI[];
+
+    private final long datosF[];
+
+    public Calculo(final Date pFechaI, final Date pFechaF) {
+        normales = 0;
         nocturnas = 0;
         dominicales = 0;
-        horasExtra = 0;
+        normalesExtra = 0;
         nocturnasExtra = 0;
         dominicaleExtra = 0;
-        inicio = pInicio;
-        fin = pFin;
+        inicio = pFechaI;
+        fin = pFechaF;
 
-        final double diferencia = fin.getTime() - inicio.getTime();
-        final double dias = diferencia / (24 * 60 * 60 * 1000);
-        final double horas = (diferencia / (60 * 60 * 1000) - dias * 24);
-        final double minutos = ((diferencia / (60 * 1000)) - dias * 24 * 60 - horas * 60);
-        horasTrabajadas = ((dias * 24 + horas) + (minutos / 60));
+        datosI = new long[]{inicio.getMinutes(), inicio.getHours(),
+                inicio.getDate(), inicio.getMonth(), inicio.getYear(),
+                darNumeroSemana(inicio)
+        };
+        datosF = new long[]{fin.getMinutes(), fin.getHours(),
+                fin.getDate(), fin.getMonth(), fin.getYear(),
+                darNumeroSemana(fin)
+        };
+
+        int contador = 0;
+        while (inicio.getTime() < fin.getTime()) {
+            inicio.setMinutes(inicio.getMinutes() + 1);
+            contador++;
+        }
+        inicio.setMinutes(inicio.getMinutes() - contador);
+        minutosTrabjados = contador;
+        horasTrabajadas = contador / 60;
+    }
+
+    private int darNumeroSemana(final Date pFecha) {
+        final Calendar calendar = new GregorianCalendar();
+        calendar.setFirstDayOfWeek(Calendar.MONDAY);
+        calendar.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
+        calendar.setMinimalDaysInFirstWeek(7);
+        calendar.setTime(pFecha);
+        return calendar.get(Calendar.WEEK_OF_YEAR);
     }
 
     public double[] darHoras() {
-        return new double[]{horas, nocturnas, dominicales, horasExtra, nocturnasExtra, dominicaleExtra};
+        return new double[]{normales, nocturnas, dominicales, normalesExtra, nocturnasExtra, dominicaleExtra};
     }
 
-    public double calcularHorasNormales(double pValor) {
-        if (pValor == 0) {
-            return 0.0;
+    private int darTipoJornada(final Date pFecha) {
+        if (pFecha.getDay() == 0) {
+            return -1;
         }
-        return 10;
+        if (pFecha.getHours() > HORA_MINIMA && pFecha.getHours() < HORA_MAXIMA) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+
+    private void calcularHoras() {
+        while (inicio.getTime() < fin.getTime()) {
+            final int tipo = darTipoJornada(inicio);
+            if (tipo == 0) {
+                normales += 1;
+            } else if (tipo == 1) {
+                nocturnas += 1;
+            } else if (tipo == -1) {
+                dominicales += 1;
+            }
+            inicio.setMinutes(inicio.getMinutes() + 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        final Date fechaI = new GregorianCalendar(2022, 1, 18, 19, 0).getTime();
+        final Date fechaF = new GregorianCalendar(2022, 1, 22, 2, 15).getTime();
+        final Calculo calculo = new Calculo(fechaI, fechaF);
+        System.out.println(calculo.minutosTrabjados);
+        System.out.println(calculo.horasTrabajadas);
+        System.out.println("----------------");
+        System.out.println(calculo.inicio);
+        System.out.println(calculo.fin);
+        System.out.println("----------------");
+        System.out.println(calculo.darNumeroSemana(calculo.inicio));
+        System.out.println(calculo.darNumeroSemana(calculo.fin));
+        System.out.println("----------------");
+
+        calculo.calcularHoras();
+        System.out.println("normales: " + calculo.normales);
+        System.out.println("nocturnas: " + calculo.nocturnas);
+        System.out.println("dominicales: " + calculo.dominicales);
     }
 }
