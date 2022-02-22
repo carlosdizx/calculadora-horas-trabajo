2ec4bf542e90b1fdb5abea5f92a62d2e422c1be6
Carlos <carlodiaz@umariana.edu.co>
2022-02-22 16:49:36 -0500

    test dos creado y funcionando con exito

--- src/test/java/ias/models/CalculadoraTest.java
+++ src/test/java/ias/models/CalculadoraTest.java
@@ -25,12 +25,30 @@ class CalculadoraTest {
     }
 
     @Test
-    void testHorasUno() {
+    void testUno() {
         setupEscenarioUno();
         final Map<String, Object> informes = calculadora.darInformes(2022, 7, reportes);
 
         assertTrue(Double.parseDouble(informes.get("normales").toString()) >= 10.6, "Las horas normales estan mal");
-        assertEquals(8 ,Double.parseDouble(informes.get("nocturnas").toString()),"Las horas nocturnas estan mal");
+        assertEquals(8, Double.parseDouble(informes.get("nocturnas").toString()), "Las horas nocturnas estan mal");
     }
 
+    @Test
+    void testDos() {
+        setupEscenarioUno();
+        Map<String, Object> informes = calculadora.darInformes(2022, 7, reportes);
+
+        assertTrue(Double.parseDouble(informes.get("normales").toString()) >= 10.6, "Las horas normales estan mal");
+        assertEquals(8, Double.parseDouble(informes.get("nocturnas").toString()), "Las horas nocturnas estan mal");
+
+        reportes.add(new Reporte(1, "1082749257", ListaServicios.A, new GregorianCalendar(2022, 1, 15, 10, 50).getTime(), new GregorianCalendar(2022, 1, 15, 18, 0).getTime()));
+        reportes.add(new Reporte(2, "1082749257", ListaServicios.B, new GregorianCalendar(2022, 1, 15, 18, 0).getTime(), new GregorianCalendar(2022, 1, 14, 20, 0).getTime()));
+        reportes.add(new Reporte(3, "1082749257", ListaServicios.C, new GregorianCalendar(2022, 1, 20, 10, 00).getTime(), new GregorianCalendar(2022, 1, 20, 18, 0).getTime()));
+
+        setupEscenarioUno();
+        informes = calculadora.darInformes(2022, 7, reportes);
+
+        assertTrue(Double.parseDouble(informes.get("normales").toString()) >= 17, "Las horas normales estan mal ");
+        assertEquals(8, Double.parseDouble(informes.get("dominicales").toString()), "Las horas dominicales estan mal");
+    }
 }
